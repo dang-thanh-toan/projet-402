edc3a47b83f405fec7424aafebc00b0f25eade04
td-en-bagaille <162378103+td-en-bagaille@users.noreply.github.com>
2026-03-19 14:35:48 +0100

    projet-402

---
+++ Assets/Scripts/PlayerPositionDebug.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerPositionDebug : MonoBehaviour
+{
+
+#if UNITY_EDITOR
+    [SerializeField]
+    private Transform debugTransform;
+
+
+    void Awake()
+    {
+        if (debugTransform)
+        {
+            transform.position = debugTransform.position;
+        }
+    }
+#endif
+}
